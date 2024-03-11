a30d1ef30d08a66ea8fdb50b5ae7cd14c77b7959
rodineyw <rodineyw@yahoo.com.br>
2024-03-10 21:29:48 -0300

    add - redesign

--- todo-list.py
+++ todo-list.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 from tkinter import messagebox
+from customtkinter import CTk, CTkButton
 
 
 def adicionar_tarefa():
+    """
+    This function adicionar_tarefa takes no parameters and does not return anything.
+    It gets the input from the variable entrada, checks if there is a value, inserts the value at the end of lista_tarefas, and then deletes the input value in entrada.
+    """
     tarefa = entrada.get()
     if tarefa:
         lista_tarefas.insert(tk.END, tarefa)
@@ -10,6 +15,9 @@ def adicionar_tarefa():
 
 
 def remover_tarefa():
+    """
+    A function to remove a task from the list of tasks.
+    """
     try:
         index = lista_tarefas.curselection()[0]
         lista_tarefas.delete(index)
@@ -18,6 +26,9 @@ def remover_tarefa():
 
 
 def editar_tarefa():
+    """
+    Save the tasks to a file named 'tarefas.txt'.
+    """
     try:
         index = lista_tarefas.curselection()[0]
         tarefa_atual = lista_tarefas.get(index)
@@ -27,8 +38,14 @@ def editar_tarefa():
     except IndexError:
         messagebox.showwarning("Atenção", "Selecione uma tarefa para editar.")
 
+    def limpar_tarefas():
+        """
+        Clear all tasks from the listbox.
+        """
+        lista_tarefas.delete(0, tk.END)
 
-root = tk.Tk()
+
+root = CTk()
 root.title("To-Do List")
 
 # Paleta de cores
@@ -51,6 +68,7 @@ lista_tarefas = tk.Listbox(
 )
 lista_tarefas.pack(side=tk.LEFT, fill=tk.BOTH)
 
+
 scrollbar = tk.Scrollbar(frame)
 scrollbar.pack(side=tk.RIGHT, fill=tk.BOTH)
 
@@ -60,36 +78,33 @@ scrollbar.config(command=lista_tarefas.yview)
 entrada = tk.Entry(root, width=40, font=("Roboto", 12))
 entrada.pack(pady=10)
 
-botao_adicionar = tk.Button(
-    root,
+botao_adicionar = CTkButton(
+    master=root,
     text="Adicionar Tarefa",
     command=adicionar_tarefa,
-    bg=cor_botao,
-    fg="white",
+    bg_color=cor_botao,
+    fg_color="white",
     font=("Roboto", 12, "bold"),
-    relief=tk.FLAT,
 )
-botao_adicionar.pack(side=tk.LEFT, padx=5)
+botao_adicionar.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-botao_remover = tk.Button(
-    root,
+botao_remover = CTkButton(
+    master=root,
     text="Remover Tarefa",
     command=remover_tarefa,
-    bg=cor_botao,
-    fg="white",
+    bg_color=cor_botao,
+    fg_color="white",
     font=("Roboto", 12, "bold"),
-    relief=tk.FLAT,
 )
 botao_remover.pack(side=tk.LEFT, padx=5)
 
-botao_editar = tk.Button(
-    root,
+botao_editar = CTkButton(
+    master=root,
     text="Editar Tarefa",
     command=editar_tarefa,
-    bg=cor_botao,
-    fg="white",
+    bg_color=cor_botao,
+    fg_color="white",
     font=("Roboto", 12, "bold"),
-    relief=tk.FLAT,
 )
 botao_editar.pack(side=tk.LEFT, padx=5)
 
